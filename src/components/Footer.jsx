2bcdca7734028967479eb8d42c41fd94b71b3aee
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-30 23:48:15 +0530

    CHANGE Color | COMPLETE Code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
 
     return (
         <footer className="footer">
-            <p>copyright © {currentYear} | All rights reserved</p>
+            <p>Copyright © {currentYear} | All rights reserved</p>
         </footer>
     );
 }
